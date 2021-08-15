ff6224690cd4e4b1c4537335c925cee662c2fe3e
abhay-jindal <jindal1808@gmail.com>
2021-08-15 12:10:35 +0530

    [ADD] basic select problems added

---
+++ Basic Select/Revising the Select Query I.sql
@@ -0,0 +1,3 @@
+﻿-- Author: Abhay Jindal
+
+SELECT * FROM city WHERE population>100000 AND countrycode='USA';
---
+++ Basic Select/Revising the Select Query II.sql
@@ -0,0 +1,3 @@
+﻿-- Author: Abhay Jindal
+
+SELECT name FROM city WHERE population>120000 AND countrycode='USA';
